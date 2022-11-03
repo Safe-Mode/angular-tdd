670a81e1987811987a42d24a5f38268be4e88eb0
Дмитриев Сергей Сергеевич <dmitriev.s@otr.ru>
2022-11-04 00:31:10 +0300

    Add book button

--- src/app/home/components/homes/homes.component.spec.ts
+++ src/app/home/components/homes/homes.component.spec.ts
@@ -4,11 +4,13 @@ import { of } from 'rxjs';
 
 import { DataService } from '../../../shared/services/data.service';
 import { HomesComponent } from './homes.component';
+import { DialogService } from '../../../shared/services/dialog.service';
 
 describe('HomeComponent', () => {
   let component: HomesComponent;
   let fixture: ComponentFixture<HomesComponent>;
   let dataService: jasmine.SpyObj<DataService>;
+  let dialogService: jasmine.SpyObj<DialogService>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -16,6 +18,9 @@ describe('HomeComponent', () => {
       providers: [{
         provide: DataService,
         useFactory: () => spyOnClass(DataService)
+      }, {
+        provide: DialogService,
+        useFactory: () => spyOnClass(DialogService)
       }]
     })
     .compileComponents();
@@ -28,6 +33,7 @@ describe('HomeComponent', () => {
 
   beforeEach(() => {
     dataService = TestBed.get(DataService);
+    dialogService = TestBed.get(DialogService);
 
     dataService.getHomes.and.returnValue(of([{
       title: 'Home 1',
@@ -60,5 +66,17 @@ describe('HomeComponent', () => {
     expect(home.querySelector('[data-test="location"]').innerText).toEqual('New York');
     expect(home.querySelector('[data-test="price"]').innerText).toEqual('$100');
     expect(home.querySelector('[data-test="image"]')).toBeTruthy();
+  });
+
+  it('should show book btn', () => {
+    const home = fixture.nativeElement.querySelector('[data-test="home"]');
+    expect(home.querySelector('[data-test="book-btn"]')).toBeTruthy();
+  });
+
+  it('should use dialog service to open book dialog, when clicking Book Btn', () => {
+    const bookBtn = fixture.nativeElement.querySelector('[data-test="book-btn"]');
+
+    bookBtn.click();
+    expect(dialogService.open).toHaveBeenCalled();
   });
 });
--- src/app/home/components/homes/homes.component.ts
+++ src/app/home/components/homes/homes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { DataService } from '../../../shared/services/data.service';
+import { DialogService } from '../../../shared/services/dialog.service';
 
 @Component({
   selector: 'app-home',
@@ -11,11 +12,16 @@ export class HomesComponent implements OnInit {
 
   homes$: Observable<any[]> | null = null;
 
-  constructor(private dataService: DataService) {
+  constructor(private dataService: DataService,
+              private dialogService: DialogService) {
   }
 
   ngOnInit(): void {
     this.homes$ = this.dataService.getHomes();
   }
 
+
+  openDialog(): void {
+    this.dialogService.open();
+  }
 }
